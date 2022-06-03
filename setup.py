2999ffa5b821032e24c884100bdae8c8470c35e0
Anton Tupitsyn <AVTupitsyn@alfabank.ru>
2022-06-03 13:22:26 +0300

    first commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='marvel_game',  # package name
     version='0.1',  # version
     description='Package Description',  # short description
-    url='http://example.com',  # package URL
+    url='https://github.com/Cbrogg/marvel-discord-game',  # package URL
     install_requires=[],  # list of packages this package depends on.
     packages=['marvel_game'],  # List of module names that installing this package will provide.
 )
